80a39694860702a651ae2fda90e6bdadbcb773bd
doubletry <654507985@qq.com>
2018-10-16 12:40:56 +0800

    use this to test the model

---
+++ scripts/test_color.sh
@@ -0,0 +1,32 @@
+set -ex
+# models
+RESULTS_DIR='./results/color2blind2'
+#G_PATH='./pretrained_models/edges2shoes_net_G.pth'
+#E_PATH='./pretrained_models/edges2shoes_net_E.pth'
+
+# dataset
+CLASS='color2blind'
+DIRECTION='AtoB' # from domain A to domain B
+LOAD_SIZE=256 # scale images to this size
+FINE_SIZE=256 # then crop to this size
+INPUT_NC=3  # number of channels in the input image
+
+# misc
+GPU_ID=0   # gpu id
+HOW_MANY=783 # number of input images duirng test
+NUM_SAMPLES=1 # number of samples per input images
+
+# command
+CUDA_VISIBLE_DEVICES=${GPU_ID} python ./test.py \
+  --dataroot ./datasets/fun  \
+  --results_dir ${RESULTS_DIR} \
+  --checkpoints_dir ./checkpoints/ \
+  --name ${CLASS} \
+  --which_direction ${DIRECTION} \
+  --loadSize ${FINE_SIZE} \
+  --fineSize ${FINE_SIZE} \
+  --input_nc ${INPUT_NC} \
+  --how_many ${HOW_MANY} \
+  --n_samples ${NUM_SAMPLES} \
+  --center_crop \
+  --no_flip
